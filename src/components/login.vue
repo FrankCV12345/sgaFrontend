a7df8108deaa7a40de68617016e16f24e4b114ed
Your Name <you@example.com>
2019-10-21 17:32:18 -0500

    agrego el login como componenete

---
+++ src/components/login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>LOGIN</h1>
+         <v-text-field
+              label="Usuario"
+              single-line
+              solo
+            ></v-text-field>
+        <v-text-field
+              label="Password"
+              single-line
+              solo
+            ></v-text-field>
+        <v-btn color="red">ENTRAR</v-btn>
+    </div>
+</template>
+<script>
+export default {
+    name : 'login',
+    data() {
+        return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+    }
+}
+</script>
