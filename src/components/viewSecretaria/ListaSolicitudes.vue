c7e2c8520ca8bb68c4d56116dd5e78e9a573a77a
Your Name <you@example.com>
2020-01-08 15:20:26 -0500

    cambio list por data table en la lista de solicitudes para secretaria

--- src/components/viewSecretaria/ListaSolicitudes.vue
+++ src/components/viewSecretaria/ListaSolicitudes.vue
@@ -6,17 +6,30 @@
             </v-col>
         </v-row>
         <v-row>
-            <v-col sm="12">
-                <v-list>
-                    <v-list-item v-for=" item in listaSolicitudes " :key="item.id" >
-                        <v-btn icon @click="VerSolicitud(item)">
-                            <v-icon>mdi-eye</v-icon>
-                        </v-btn>
-                        <v-list-item-content>
-                            <v-list-item-title>{{llenaCeros(item.id,5,'SOL')}} <v-spacer></v-spacer>  Tipo :{{item.tiposolicitud.nombresolicitud}}</v-list-item-title>
-                        </v-list-item-content>
-                    </v-list-item>
-                </v-list>
+            <v-col sm="8"></v-col>
+            <v-col sm="4" >
+                <v-text-field
+                    v-model="search"
+                    append-icon="mdi-glasses"
+                    label="Buscar"
+                    single-line
+                    hide-details
+                ></v-text-field>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col sm="12" >
+                <v-data-table
+                    :headers="headers"
+                    :items="listaSolicitudes"
+                    :search="search"
+                >
+                <template v-slot:item.action={item} >
+                    <v-btn icon @click="VerSolicitud(item)">
+                        <v-icon>mdi-eye</v-icon>
+                    </v-btn>
+                </template>
+                </v-data-table>    
             </v-col>
         </v-row>
         <v-dialog  v-model="dialogSolictud" scrollable persistent max-width="980px" >
@@ -61,7 +74,23 @@ export default {
             listaSolicitudes:[],
             dialogSolictud:false,
             solicitudSelected : null,
-            listaTiposEstado :null
+            listaTiposEstado :null,
+            headers:[
+                {
+                    text: 'Solicitud',
+                    value: 'idConCeros',
+                },
+                {
+                    text: 'Tipo Solicitud',
+                    value: 'tiposolicitud.nombresolicitud',
+                },
+                { 
+                    text: '', 
+                    value: 'action',
+                    sortable: false 
+                }
+            ],
+            search:''
         }
     },
     methods:{
@@ -69,6 +98,13 @@ export default {
             s_ListaSolicitdes().then(
                 response =>{
                     this.listarEstadosSolicitud()
+                    for(let i=0; i < response.data.length; i++){
+                        response.data[i].idConCeros =  this.llenaCeros( 
+                            response.data[i].id,
+                            5,
+                            'SOL'
+                            )
+                    }
                     this.listaSolicitudes =  response.data
                 }).catch(
                 error =>{
